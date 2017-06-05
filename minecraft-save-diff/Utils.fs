fe3c97e778df7d15382d436c9e7d81caebe167dd
koru1130 <koru1130@gmail.com>
2017-06-05 23:48:35 +0800

    add decompress

---
+++ minecraft-save-diff/Utils.fs
@@ -0,0 +1,17 @@
+﻿namespace MinecraftSaveDiff
+
+open System.IO
+
+module Utils =
+        
+    let streamToArray (stream:Stream) =
+        let outputStream = new MemoryStream()
+        stream.CopyTo(outputStream)
+        outputStream.ToArray()
+
+    let zlibDecompress (byteArray:byte[]) =
+        new Compression.DeflateStream(new MemoryStream(byteArray.[2..]),Compression.CompressionMode.Decompress)
+        |>streamToArray
+    let gzipDecompress (byteArray:byte[]) =
+        new Compression.GZipStream(new MemoryStream(byteArray),Compression.CompressionMode.Decompress)
+        |>streamToArray
